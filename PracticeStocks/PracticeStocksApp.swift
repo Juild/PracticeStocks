eb12d08ea9a1b18aaedd66329bf167250731ef7a
Juild <jcbr4696@gmail.com>
2023-08-15 11:15:53 +0200

    Initial Commit

---
+++ PracticeStocks/PracticeStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeStocksApp.swift
+//  PracticeStocks
+//
+//  Created by Jc on 15/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
